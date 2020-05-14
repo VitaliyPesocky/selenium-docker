7997d1514ee517e35ab10db0ab16a5528374ec3f
VitaliyPesocky <vitaliypesocky@gmail.com>
2020-05-14 22:26:17 +0300

    added FindFlightPage

---
+++ src/main/java/com/newtours/pages/FindFligthPage.java
@@ -0,0 +1,35 @@
+package com.newtours.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FindFligthPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(name = "reserveFlights")
+    private WebElement continueButton;
+    @FindBy(name = "reserveFlights")
+    private WebElement securePurchaseButton;
+
+    public FindFligthPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 30);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void submitFindFlightPage() {
+        this.wait.until(ExpectedConditions.elementToBeClickable(this.continueButton));
+        this.continueButton.click();
+    }
+
+    public void goToOrderConfirmationPage() {
+        this.wait.until(ExpectedConditions.elementToBeClickable(this.securePurchaseButton));
+        this.securePurchaseButton.click();
+    }
+}
